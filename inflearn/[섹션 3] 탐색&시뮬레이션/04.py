5923c9987d2d6210c7d7de1cbbb3f9d1e126a00e
JoohyunLim <jjuha.dev@gmail.com>
2022-02-07 17:53:55 +0900

    [섹션 3] 4 문제풀이

--- inflearn/[섹션 3] 탐색&시뮬레이션/04.py
+++ inflearn/[섹션 3] 탐색&시뮬레이션/04.py
@@ -3,6 +3,6 @@ list1 = list(map(int, input().split()))
 n2 = int(input())
 list2 = list(map(int, input().split()))
 # list1 뒤에 list2를 붙인 후 정렬
-list1 = sorted(list1.__add__(list2))
+list1 = sorted(list1+list2)
 for i in list1:
     print(i,end=" ")
