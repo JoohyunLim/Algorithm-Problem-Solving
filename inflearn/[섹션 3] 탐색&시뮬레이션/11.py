3dc1bfa892beedcc08ec8d3cc2da68089d6bf5ec
JoohyunLim <jjuha.dev@gmail.com>
2022-02-23 18:45:13 +0900

    [섹션 3] 11 문제풀이

--- inflearn/[섹션 3] 탐색&시뮬레이션/11.py
+++ inflearn/[섹션 3] 탐색&시뮬레이션/11.py
@@ -1,12 +1,14 @@
 board=[list(map(int, input().split())) for _ in range(7)]
 cnt=0
+len=5
 for i in range(3):
     for j in range(7):
-        tmp=board[j][i:i+5]
+        tmp=board[j][i:i+len]
         if tmp==tmp[::-1]:
             cnt+=1
-        for k in range(2):
-            if board[i+k][j]!=board[i+5-k-1][j]:
+        #tmp=board[i:i+5][j] 앞 행은 리스트가 아니라서 슬라이스가 안된다.
+        for k in range(len//2):
+            if board[i+k][j]!=board[len-k+i-1][j]:
                 break;
         else:
             cnt+=1
