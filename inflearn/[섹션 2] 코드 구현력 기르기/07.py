ad3dbdafa7c5250094460ae7af9ed541d2257123
JoohyunLim <jjuha.dev@gmail.com>
2022-02-24 21:15:35 +0900

    [섹션 2] 07 문제풀이(2)

--- inflearn/[섹션 2] 코드 구현력 기르기/07.py
+++ inflearn/[섹션 2] 코드 구현력 기르기/07.py
@@ -10,3 +10,16 @@ for i in range(2, n+1):
         for j in range(i+i, n+1, i):  #소수 i의 배수들은 모두 제외(False)
             numList[j] = False;
 print(cnt)
+
+"""
+#풀이2
+n=int(input())
+ch=[0]*(n+1)
+cnt=0	
+for i in range(2, n+1):
+    if ch[i]==0:    
+        cnt+=1
+        for j in range(i, n+1, i):  
+            ch[j]=1
+print(cnt)
+"""
